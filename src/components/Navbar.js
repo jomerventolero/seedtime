b59220d7d2f7d45c78df08eae28dfc536a449bc2
orekihoutaro <orekihoutaro1218@gmail.com>
2023-08-16 23:07:36 +0800

    Menu updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,16 @@
 "use client";
-import React from 'react'
-import logo from 'public/logo.png'
-import brandfb from 'public/brand-facebook.png'
+import React, { useState } from 'react';
+import logo from 'public/logo.png';
+import brandfb from 'public/brand-facebook.png';
 import Image from 'next/image';
 import { ImMenu } from 'react-icons/im';
-import { useState } from 'react';
-
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
-  const menu = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const handleClick = () => {
-    menu.current = !menu.current;
-
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   }
 
   return (
@@ -30,17 +28,30 @@ const Navbar = () => {
                 <a href="https://facebook.com/SHCIFAMILY">
                     <Image src={brandfb} alt="brand-fb" />
                 </a>
-                <ImMenu className="cursor-pointer w-[64px] h-[32px]" onClick={handleClick}/>
-                {menu.current && (
-                    <div className="absolute top-0 left-0 w-full h-screen bg-black bg-opacity-50">
-                        Opened
-                    </div>
+                <ImMenu className="cursor-pointer w-[64px] h-[32px]" onClick={toggleMenu}/>
+                {isMenuOpen && (
+                    <motion.div 
+                        initial={{ opacity: 0, y: -50 }}
+                        animate={{ opacity: 1, y: 0}}
+                        exit={{ opacity: 0, y: -50 }}
+                        transition={{
+                            type: "fadeIn",
+                            duration: 1,
+                            delay: 0.1
+                        }}
+                        className="absolute top-0 right-0 mt-16 mr-4 w-40 p-2 bg-white border border-gray-300 shadow">
+                        <ul className="space-y-2">
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#events">Events</a></li>
+                            {/* Add more menu items as needed */}
+                        </ul>
+                    </motion.div>
                 )} 
             </div>
-
         </nav>
     </section>
   )
 }
 
-export default Navbar
+export default Navbar;
